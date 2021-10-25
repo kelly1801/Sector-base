5f67f1d06bf297a600fb7391f2fdd9118676abf7
Kelly Chara <fabianachara@gmail.com>
2021-10-24 22:52:50 -0500

    all sections complete left final touch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -6,8 +8,30 @@ module.exports = {
       backgroundImage: {
         'sector': "url('../images/img-2.png')"
         
-       }
+       },
 
+       colors: {
+      'dark-blue': '#0B1D26',
+      "brigth-yellow": '#FDD130'
+      },
+
+      height: {
+        'hst': '720px',
+        
+       },    
+       
+       width: {
+        'wst': '566px',
+        
+      },
+
+      fontSize: {
+        'xxs': ['18px', '32px'],
+        'm':['64px', '20px'],
+        'big': ['240px', '20px'],
+        'mm': '124px',
+        
+      }
     },
   },
   variants: {
